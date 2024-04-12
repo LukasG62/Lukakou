92a1a52a38a678e4ababc6d4426b1903389b2d0c
LukasG62 <lukas.grando.62@gmail.com>
2024-04-13 01:47:29 +0200

    unit_test(lexer) - Add first version of unit test for lexer (may change)

---
+++ test/test_lexer.c
@@ -0,0 +1,19 @@
+#include "common.h"
+#include "lexer.h"
+
+
+int main(int argc, char *argv[]) {
+    DEBUG_PRINT("Lexer test\n");
+    
+    char *source = "/* Multiple\nLine\nComment\n*/\n // Single comment\n int start() {\nstring chaine = \"bababoi\"\nint toto = 1230 \n\treturn 0 // return 0 like a bozo \n}\n";
+
+    lexer_t lexer = create_lexer(source, strlen(source));
+    token_t token = next_token(&lexer);
+    
+    while (token.type != TOKEN_EOF) {
+        printf("[%s]\t [%.*s]\n", str_token_type(token.type), (int)token.textLen, token.text);
+        token = next_token(&lexer);
+    }
+    
+    return EXIT_SUCCESS;
+}
