3f4d85ff9a94aad345386b227c6d1a1fc65d73af
LukasG62 <lukas.grando.62@gmail.com>
2024-04-14 00:53:50 +0200

    feat(test_lexer) - Test with operators

--- test/test_lexer.c
+++ test/test_lexer.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     DEBUG_PRINT("Lexer test\n");
     
-    char *source = "/* Multiple\nLine\nComment\n*/\n // Single comment\n int start() {\nstring chaine = \"bababoi\"\nint toto = 1230 \n\treturn 0 // return 0 like a bozo \n}\n";
+    char *source = "/* Multiple\nLine\nComment\n*/\n // Single comment\n int start() {\n+= -= *= /= OR AND NOT == >= <= > \nstring chaine = \"bababoi\"\nint toto = 1 OR 2 AND 3 NOT 4 \n\treturn 0 // return 0 like a bozo \n}\n";
 
     lexer_t lexer = create_lexer(source, strlen(source));
     token_t token = next_token(&lexer);
